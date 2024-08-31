5af558b0ace892286ef4bcd95f87817ca34a115a
Sanjay Singh <sanjay007abd@gmail.com>
2024-08-31 23:18:32 +0530

    updating our app with shop page and style that

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate, useLocation, useParams, useMatch } from 'react-router-dom';
+import './menu-item.styles.scss';
 
-import './menu-item.styles.scss'
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const match = useMatch(location.pathname);
 
+  const handleClick = () => {
+    navigate(`${match.pathname}${linkUrl}`);
+  };
 
-
-const MenuItem  = ({title, imageUrl,size}) => (
-    <div className={`${size} menu-item`}>
-    <div className="background-image" style = {{
-        backgroundImage: `url(${imageUrl})`
-    }}/>
-                <div className="content">
-                    <h1 className="title">{title.toUpperCase()}</h1>
-                    <span className="subtitle">SHOP NOW</span>
-
-                </div>
-            </div>
-)
+  return (
+    <div className={`${size} menu-item`} onClick={handleClick}>
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
 
 export default MenuItem;
