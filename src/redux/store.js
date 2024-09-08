092573ea3acac22072e7f7fb199ac3509cf3467c
Sanjay Singh <sanjay007abd@gmail.com>
2024-09-08 23:32:14 +0530

    updating our app to redirectt to home page

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,18 @@
 
-import { configureStore } from '@reduxjs/toolkit'
-import logger from 'redux-logger';
-
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import rootReducer from './root-reducer';
+import logger from 'redux-logger';
 
 const middlewares = [logger];
 
-const store = configureStore({reducer:rootReducer})
+const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // Disable serializable check
+    }).concat(middlewares),
+  devTools: process.env.NODE_ENV !== 'production',
+});
 
 export default store;
+ 
