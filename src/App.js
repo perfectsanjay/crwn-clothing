b6ba57e99551778501fc2445d1dbd9740a5df483
Sanjay Singh <sanjay007abd@gmail.com>
2024-09-11 15:56:28 +0530

    added checkout page, checkout item component and updated cart to use selectors

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { Routes, Route,Navigate } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
 
 import './App.css';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import CheckoutPage from './pages/checkout/checkout.component.jsx'
+
+
 import Header from './components/header/header.component.jsx';
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
 import { getDoc } from 'firebase/firestore';
 import { setCurrentUser } from './redux/user/user.actions.js';
+import { selectCurrentUser } from './redux/user/user.selectors.js';
 
 class App extends React.Component {
 
@@ -46,6 +54,7 @@ class App extends React.Component {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/shop" element={<ShopPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
           <Route path="/signIn" element={this.props.currentUser ? (<Navigate to='/' />) : (<SignInAndSignUpPage/>)} />
         </Routes>
       </div>
@@ -53,8 +62,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 
 const mapDispatchToProps = (dispatch) => ({
