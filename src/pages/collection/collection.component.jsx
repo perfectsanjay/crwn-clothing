0e1ac7742beed8bb78a5474543348d0943bb4c48
Sanjay Singh <sanjay007abd@gmail.com>
2024-09-19 22:30:10 +0530

    updating stripe integration and updating our favicon and title

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -23,7 +23,7 @@ const CollectionPage = () => {
       <h2 className="title">{title}</h2>
       <div className="items">
         {items.map((item) => (
-          <CollectionItem key={item.id} item={item} />
+          <CollectionItem key={item.id} item={item} /> 
         ))}
       </div>
     </div>
